8fdcae6110506f648f7e5a095ac48b76d23d1da6
theblessedcity <theblessedcity@hanmail.net>
2025-02-24 17:57:27 +0900

    feature: add routes for service

--- packages/service/src/Routes/routes.tsx
+++ packages/service/src/Routes/routes.tsx
@@ -4,6 +4,13 @@ const Page = () => {
   return null;
 };
 
-const routes = [{ url: "", element: <Page /> }];
+const routes = [
+  { url: "/", element: <Page /> },
+  { url: "/nickname", element: <Page /> },
+  { url: "/interpret", element: <Page /> },
+  { url: "/result", element: <Page /> },
+  { url: "/relief", element: <Page /> },
+  { url: "/analysis", element: <Page /> },
+];
 
 export default routes;
