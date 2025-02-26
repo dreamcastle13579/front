00dccf08940384e510c3e1cdfa7b9ab6813dae42
theblessedcity <theblessedcity@hanmail.net>
2025-02-26 15:21:09 +0900

    feature: export components

---
+++ packages/design-system/src/index.ts
@@ -0,0 +1,19 @@
+import { Input } from "./components/Input";
+import { Textarea } from "./components/Textarea";
+import { Button } from "./components/Button";
+import { Title } from "./components/Title";
+import { Bubble } from "./components/Bubble";
+import { DigitText } from "./components/DigitText";
+import { ContentBox } from "./components/ContentBox";
+import { TextBox } from "./components/TextBox";
+
+export {
+  Input,
+  Textarea,
+  Button,
+  Title,
+  Bubble,
+  DigitText,
+  ContentBox,
+  TextBox,
+};
