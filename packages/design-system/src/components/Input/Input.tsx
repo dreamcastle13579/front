c9dbb8f1c73e1809708daa2b48feae259a69c2d9
theblessedcity <theblessedcity@hanmail.net>
2025-02-26 15:13:45 +0900

    feature: add ref to Input

--- packages/design-system/src/components/Input/Input.tsx
+++ packages/design-system/src/components/Input/Input.tsx
@@ -5,22 +5,23 @@ import "./Input.scss";
 interface Props {
   value: string;
   placeholder?: string;
-  onChange?: (e: React.ChangeEvent) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   align?: "center";
 }
 
-const Input = (props: Props) => {
+const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
   const { value, placeholder, onChange, align } = props;
   return (
     <div className={`ui-input ${align ? `align-${align}` : ""}`}>
       <input
+        ref={ref}
         value={value}
         {...(placeholder ? { placeholder } : {})}
         onChange={onChange}
       />
     </div>
   );
-};
+});
 
 export type { Props };
 export default Input;
