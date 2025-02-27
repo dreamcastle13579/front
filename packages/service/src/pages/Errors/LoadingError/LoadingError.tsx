001e9ae5724d237cad1e39301ef297a78351a0b7
theblessedcity <theblessedcity@hanmail.net>
2025-02-27 11:35:12 +0900

    feature: add LoadingError page

--- packages/service/src/pages/Errors/LoadingError/LoadingError.tsx
+++ packages/service/src/pages/Errors/LoadingError/LoadingError.tsx
@@ -1,13 +1,46 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./LoadingError.scss";
 
 import { Layout, Main } from "../../../frame";
 
+import { FullPageInfo, Button } from "design-system";
+
+import ImgFairy from "./imgs/img-fairy.svg";
+
 const LoadingError = () => {
+  const navigate = useNavigate();
+  const handelMoveToPage = () => {
+    navigate("/nickname");
+  };
+
   return (
     <Layout>
-      <Main>loading error</Main>
+      <Main>
+        <FullPageInfo
+          info="loading-error"
+          before={{
+            title: (
+              <>
+                앗, 뭔가 잘못됐다꿈!
+                <br /> 다시 시도해봐라꿈!
+              </>
+            ),
+            img: <img src={ImgFairy} alt="" />,
+            content: (
+              <Button
+                variant="primary"
+                onClick={() => {
+                  handelMoveToPage();
+                }}
+              >
+                뒤로가기
+              </Button>
+            ),
+          }}
+        />
+      </Main>
     </Layout>
   );
 };
