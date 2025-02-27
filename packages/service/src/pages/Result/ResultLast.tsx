2e74a2789f7de4b1b408a924584b291f99e40f9b
theblessedcity <theblessedcity@hanmail.net>
2025-02-27 13:57:27 +0900

    feature: fixed-bottom for ResultLast

--- packages/service/src/pages/Result/ResultLast.tsx
+++ packages/service/src/pages/Result/ResultLast.tsx
@@ -12,19 +12,25 @@ const Result = () => {
   };
   return (
     <>
-      <Title title="style">우리의 해석은 여기까지라꿈!</Title>
-      <Bead />
-      <TextBox>
-        별명 위해 특별히
-        <br /> 소원 구슬을 준비했꿈! <br />
-        <br />
-        원하는 것을 빌어봐라꿈
-      </TextBox>
-      <ButtonBox>
-        <Button variant="primary" onClick={handelMoveToPage}>
-          오 해볼래
-        </Button>
-      </ButtonBox>
+      <div className="full-box result-full-box">
+        <div className="full-box-content">
+          <Title title="style">우리의 해석은 여기까지라꿈!</Title>
+          <Bead />
+          <TextBox>
+            별명 위해 특별히
+            <br /> 소원 구슬을 준비했꿈! <br />
+            <br />
+            원하는 것을 빌어봐라꿈
+          </TextBox>
+        </div>
+        <div className="full-box-bottom">
+          <ButtonBox>
+            <Button variant="primary" onClick={handelMoveToPage}>
+              오 해볼래
+            </Button>
+          </ButtonBox>
+        </div>
+      </div>
     </>
   );
 };
