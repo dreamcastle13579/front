5855a48927718183130eca44bbf529e98719589a
theblessedcity <theblessedcity@hanmail.net>
2025-02-27 23:05:16 +0900

    feature: add adjustable props to Textarea

--- packages/design-system/src/components/Textarea/Textarea.stories.tsx
+++ packages/design-system/src/components/Textarea/Textarea.stories.tsx
@@ -17,3 +17,10 @@ Exmaple.args = {
   value:
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, et voluptatibus nemo excepturi mollitia unde, neque deserunt aspernatur ullam nobis incidunt, repellendus id nulla natus. Unde reiciendis commodi quia veniam!",
 };
+
+export const Adjustable = Template.bind({});
+Adjustable.args = {
+  value:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, et voluptatibus nemo excepturi mollitia unde, neque deserunt aspernatur ullam nobis incidunt, repellendus id nulla natus. Unde reiciendis commodi quia veniam!",
+  adjustable: true,
+};
--- packages/design-system/src/components/Textarea/Textarea.tsx
+++ packages/design-system/src/components/Textarea/Textarea.tsx
@@ -7,23 +7,36 @@ interface Props {
   placeholder?: string;
   maxLength?: number;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  adjustable?: boolean;
 }
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, Props>(
-  (props: Props) => {
-    const { value, placeholder, maxLength, onChange } = props;
-    return (
-      <div className={`ui-textarea`}>
-        <textarea
-          value={value}
-          {...(placeholder ? { placeholder } : {})}
-          {...(maxLength ? { maxLength } : {})}
-          onChange={onChange}
-        ></textarea>
-      </div>
-    );
-  }
-);
+const Textarea = React.forwardRef<HTMLTextAreaElement, Props>((props, ref) => {
+  const { value, placeholder, maxLength, onChange, adjustable } = props;
+
+  const handleAdjustableHeight = () => {
+    const textarea = ref?.current;
+    if (textarea) {
+      textarea.style.height = "0px";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    }
+  };
+
+  React.useEffect(() => {
+    if (adjustable) handleAdjustableHeight();
+  }, [value, adjustable]);
+  return (
+    <div className={`ui-textarea`}>
+      <textarea
+        rows={1}
+        value={value}
+        {...(placeholder ? { placeholder } : {})}
+        {...(maxLength ? { maxLength } : {})}
+        onChange={onChange}
+        ref={ref}
+      ></textarea>
+    </div>
+  );
+});
 
 export type { Props };
 export default Textarea;
