9385c466f620fe4c178e66747378cb7c92b9396d
theblessedcity <theblessedcity@hanmail.net>
2025-02-27 22:19:41 +0900

    feature: add maxLength props to Input, Textarea

--- packages/design-system/src/components/Input/Input.tsx
+++ packages/design-system/src/components/Input/Input.tsx
@@ -5,18 +5,20 @@ import "./Input.scss";
 interface Props {
   value: string;
   placeholder?: string;
+  maxLength?: number;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   align?: "center";
 }
 
 const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
-  const { value, placeholder, onChange, align } = props;
+  const { value, placeholder, maxLength, onChange, align } = props;
   return (
     <div className={`ui-input ${align ? `align-${align}` : ""}`}>
       <input
         ref={ref}
         value={value}
         {...(placeholder ? { placeholder } : {})}
+        {...(maxLength ? { maxLength } : {})}
         onChange={onChange}
       />
     </div>
--- packages/design-system/src/components/Textarea/Textarea.tsx
+++ packages/design-system/src/components/Textarea/Textarea.tsx
@@ -5,17 +5,19 @@ import "./Textarea.scss";
 interface Props {
   value: string;
   placeholder?: string;
+  maxLength?: number;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, Props>(
   (props: Props) => {
-    const { value, placeholder, onChange } = props;
+    const { value, placeholder, maxLength, onChange } = props;
     return (
       <div className={`ui-textarea`}>
         <textarea
           value={value}
           {...(placeholder ? { placeholder } : {})}
+          {...(maxLength ? { maxLength } : {})}
           onChange={onChange}
         ></textarea>
       </div>
