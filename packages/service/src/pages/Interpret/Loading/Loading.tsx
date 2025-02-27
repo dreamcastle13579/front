7158d2eddaf4bbd02381db784f7050e337d2ee2b
theblessedcity <theblessedcity@hanmail.net>
2025-02-27 23:23:49 +0900

    fix: use same img for Loading

--- packages/service/src/pages/Interpret/Loading/Loading.tsx
+++ packages/service/src/pages/Interpret/Loading/Loading.tsx
@@ -5,7 +5,8 @@ import "./Loading.scss";
 import { FullPageInfo, Button } from "design-system";
 
 import LoadingBefore from "./imgs/loading-before.svg";
-import LoadingAfter from "./imgs/loading-after.svg";
+
+const LoadingImg = <img src={LoadingBefore} alt="" />;
 
 const Loading = () => {
   return (
@@ -13,7 +14,7 @@ const Loading = () => {
       info="loading"
       before={{
         title: "꿈을 읽고 있꿈..",
-        img: <img src={LoadingBefore} alt="" />,
+        img: LoadingImg,
         content: (
           <div className="loading-before">
             지금 페이지를 나가면 내용을 잃을 수 있어요
@@ -23,7 +24,7 @@ const Loading = () => {
       }}
       after={{
         title: "별명! 오늘 너의 꿈은...",
-        img: <img src={LoadingAfter} alt="" />,
+        img: LoadingImg,
         content: <Button variant="primary">어떤 꿈인데?</Button>,
       }}
     />
