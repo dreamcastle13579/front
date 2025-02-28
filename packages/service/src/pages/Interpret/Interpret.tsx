fe15b8197fc1a18872c95e888f3c97aea5b19c21
theblessedcity <theblessedcity@hanmail.net>
2025-02-28 11:29:27 +0900

    feature: set content in Interpret

--- packages/service/src/pages/Interpret/Interpret.tsx
+++ packages/service/src/pages/Interpret/Interpret.tsx
@@ -16,10 +16,28 @@ import {
   FullFixedBox,
 } from "design-system";
 
+import { AppContext } from "../../context/AppContext";
+import { DreamState } from "../../context/AppContext";
+
 const Interpret = () => {
   const navigate = useNavigate();
   const handelMoveToPage = () => {
     navigate("/result/1");
+  };
+
+  const { setDream } = React.useContext(AppContext);
+  const handleClick = () => {
+    setDream((prev) => {
+      if (prev === null) return null;
+      return { ...prev, content } as DreamState;
+    });
+
+    handelMoveToPage();
+  };
+
+  const [content, setContent] = React.useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
   };
 
   return (
@@ -31,7 +49,7 @@ const Interpret = () => {
             bottomSpace={56}
             bottom={
               <ButtonBox>
-                <Button variant="primary" onClick={handelMoveToPage}>
+                <Button variant="primary" onClick={handleClick}>
                   이게 내 꿈이야
                 </Button>
               </ButtonBox>
@@ -40,11 +58,11 @@ const Interpret = () => {
             <Title title="info">오늘 내가 꾼 꿈은...</Title>
             <Bubble bubble="textarea">
               <Textarea
-                value=""
+                value={content}
                 placeholder="오늘 꾼 꿈을 입력해주세요"
-                onChange={() => {}}
+                onChange={handleChange}
               ></Textarea>
-              <DigitText total="250">282</DigitText>
+              <DigitText total="250">280</DigitText>
             </Bubble>
           </FullFixedBox>
         </ContentBox>
