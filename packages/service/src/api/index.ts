1e3baebc9f6df813ca3fd76a4daf73a5af10fadd
theblessedcity <theblessedcity@hanmail.net>
2025-02-28 10:17:55 +0900

    chore: use env variables for api server ip

--- packages/service/src/api/index.ts
+++ packages/service/src/api/index.ts
@@ -3,13 +3,19 @@ import axios from "axios";
 const instance = axios.create();
 
 export const postInterpret = ({
+  promptType,
   nickname,
   content,
 }: {
+  promptType?: string;
   nickname: string;
   content: string;
 }) => {
-  return axios.post("/dreams/interpret", { data: { nickname, content } });
+  return axios.post(`${import.meta.env.VITE_BASE_URL}/dreams/interpretation`, {
+    promptType,
+    nickname,
+    content,
+  });
 };
 
 export default instance;
