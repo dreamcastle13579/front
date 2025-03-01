a8dc64064c040c10fc1d0d2c73d5074dc170fa49
theblessedcity <theblessedcity@hanmail.net>
2025-03-01 23:12:05 +0900

    fix: add options paremater for api

--- packages/service/src/api/index.ts
+++ packages/service/src/api/index.ts
@@ -2,20 +2,29 @@ import axios from "axios";
 
 const instance = axios.create();
 
-export const postInterpret = ({
-  promptType,
-  nickname,
-  content,
-}: {
-  promptType?: string;
-  nickname: string;
-  content: string;
-}) => {
-  return axios.post(`${import.meta.env.VITE_BASE_URL}/dreams/interpretation`, {
+export const postInterpret = (
+  {
     promptType,
     nickname,
     content,
-  });
+  }: {
+    promptType?: string;
+    nickname: string;
+    content: string;
+  },
+  options?: {
+    timeout?: number;
+  }
+) => {
+  return axios.post(
+    `${import.meta.env.VITE_BASE_URL}/dreams/interpretation`,
+    {
+      promptType,
+      nickname,
+      content,
+    },
+    options
+  );
 };
 
 export default instance;
