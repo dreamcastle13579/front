b6b6744c752579b0cd92a23a415c768e8ed16249
theblessedcity <theblessedcity@hanmail.net>
2025-03-01 14:49:12 +0900

    chore: set mkcert for https

--- packages/service/vite.config.ts
+++ packages/service/vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   resolve: {
     alias: {
       "design-system": path.resolve(__dirname, "../design-system/src"),
